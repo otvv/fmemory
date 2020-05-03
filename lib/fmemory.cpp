8cdf1f3cd940d01da021ff125bba2b4410449e9b
otvv <suizld@pm.me>
2020-05-03 12:25:11 -0300

    v0.0.2-2
    Fixed a bug where: "getAbsoluteAddress" wouldn't accept more than 1 argument.
    Refactored some parts of the code.
    Added the ability to parse memory region of a process. (For now this is done internally.)
    Now the sigscanner takes 4 arguments. (Module name, process id, signature pattern and mask.)
    Done more work torwards making the  sig-scanner useable.
    Bumped package version.

--- lib/fmemory.cpp
+++ lib/fmemory.cpp
@@ -2,7 +2,7 @@
 //
 // by: otvv
 // license: MIT
-// 
+//
 
 #include "fmemory.hpp"
 
@@ -12,7 +12,7 @@ namespace FMEMORY
 {
 
 //-------------------------------------------------------------------//
-void* MEMORY_MAP::FindSignature(SMemoryManager_t _handle, const char *_data, const char *_pattern)
+void *MEMORY_MAP::FindSignature(SMemoryManager_t _handle, const char *_data, const char *_pattern)
 {
   char cBuffer[MAX_BUFFER_SIZE];
 
@@ -20,7 +20,6 @@ void* MEMORY_MAP::FindSignature(SMemoryManager_t _handle, const char *_data, con
   std::size_t ulChunkSize = sizeof(cBuffer);
   std::size_t ulTotalSize = (m_ulEnd - m_ulStart);
   std::size_t ulChunkIndex = 0;
-  std::size_t ulMatches;
 
   while (ulTotalSize)
   {
@@ -29,21 +28,19 @@ void* MEMORY_MAP::FindSignature(SMemoryManager_t _handle, const char *_data, con
 
     bzero(cBuffer, ulChunkSize);
 
-    if (_handle.ReadProcessMemory(reinterpret_cast<void*>(ulReadAddress), cBuffer, ulReadSize))
+    if (_handle.ReadProcessMemory(reinterpret_cast<void *>(ulReadAddress), cBuffer, ulReadSize))
     {
-      for (std::size_t b = 0; b < ulReadSize; b++)
+      for (std::size_t i = 0; i < ulReadSize; i++)
       {
-        for (std::size_t t = b; t < ulReadSize; t++) 
+        std::size_t ulMatches = 0;
+
+        while (cBuffer[i + ulMatches] == _data[ulMatches] || _pattern[ulMatches] != 'x')
         {
-          if (cBuffer[t] != _data[ulMatches] && _pattern[ulMatches] == 'x')
-          {
-            ulMatches = 0;
-            break;
-          }
+          ulMatches++;
 
           if (ulMatches == ulLength)
           {
-            return reinterpret_cast<char *>((ulReadAddress + t) - (ulMatches + 0x1));
+            return reinterpret_cast<char*>(ulReadAddress + i);
           }
         }
       }
@@ -131,7 +128,7 @@ std::uintptr_t MANAGER::GetModule(const std::string &_module_name, std::int32_t 
 }
 
 //-------------------------------------------------------------------//
-bool MANAGER::ReadProcessMemory(void* _address, void* _buffer, std::size_t _size)
+bool MANAGER::ReadProcessMemory(void *_address, void *_buffer, std::size_t _size)
 {
   struct iovec local_iovec[1];
   struct iovec remote_iovec[1];
@@ -145,7 +142,7 @@ bool MANAGER::ReadProcessMemory(void* _address, void* _buffer, std::size_t _size
 }
 
 //-------------------------------------------------------------------//
-bool MANAGER::WriteProcessMemory(void* _address, void* _buffer, std::size_t _size)
+bool MANAGER::WriteProcessMemory(void *_address, void *_buffer, std::size_t _size)
 {
   struct iovec local_iovec[1];
   struct iovec remote_iovec[1];
@@ -172,16 +169,117 @@ std::uintptr_t MANAGER::GetCallAddress(std::uintptr_t _address)
 }
 
 //-------------------------------------------------------------------//
-std::uintptr_t MANAGER::GetAbsoluteAddress(std::uintptr_t _address, int _offset, int _size)
+std::uintptr_t MANAGER::GetAbsoluteAddress(std::uintptr_t _address, std::size_t _offset, std::int32_t _size)
 {
   std::size_t ulCode = 0;
 
   if (MANAGER::ReadProcessMemory(reinterpret_cast<char *>(_address + _offset), &ulCode, sizeof(std::size_t)))
-  { 
-		return ulCode + (_address + _size);
-	}
+  {
+    return ulCode + (_address + _size);
+  }
 
   return 0;
+}
+
+//-------------------------------------------------------------------//
+void MANAGER::MapProcessMemoryRegions(std::int32_t _process_id)
+{
+  m_prgpMemoryMap.clear();
+  std::ifstream cMaps("/proc/" + std::to_string(_process_id) + "/maps");
+
+  std::string strLine;
+  while (std::getline(cMaps, strLine))
+  {
+    std::istringstream ssStream(strLine);
+
+    std::string strMemorySpace, strPermissions, strOffset, strDevice, strNode;
+
+    if (ssStream >> strMemorySpace >> strPermissions >> strOffset >> strDevice >> strNode)
+    {
+      std::string strPathName;
+
+      for (std::size_t ls = 0, i = 0; i < strLine.length(); i++)
+      {
+        if (strLine.substr(i, 1).compare(" ") == 0)
+        {
+          ls++;
+
+          if (ls == 5)
+          {
+
+            std::int32_t iBegin = strLine.substr(i, strLine.size()).find_first_not_of(' ');
+
+            if (iBegin != -1)
+            {
+              strPathName = strLine.substr(iBegin + i, strLine.size());
+            }
+            else
+            {
+              strPathName.clear();
+            }
+          }
+        }
+      }
+
+      MEMORY_MAP memRegion;
+      std::stringstream ssSecondStream;
+
+      std::int32_t iMemorySplit = strMemorySpace.find_first_of('-');
+
+      if (iMemorySplit != -1)
+      {
+        ssSecondStream << std::hex << strMemorySpace.substr(0, iMemorySplit);
+        ssSecondStream >> memRegion.m_ulStart;
+        ssSecondStream.clear();
+        ssSecondStream << std::hex << strMemorySpace.substr(iMemorySplit + 1, strMemorySpace.size());
+        ssSecondStream >> memRegion.m_ulEnd;
+        ssSecondStream.clear();
+      }
+
+      std::int32_t iDeviceSplit = strDevice.find_first_of(':');
+
+      if (iDeviceSplit != -1)
+      {
+        ssSecondStream << std::hex << strDevice.substr(0, iDeviceSplit);
+        ssSecondStream >> memRegion.m_ucDeviceMajor;
+        ssSecondStream.clear();
+        ssSecondStream << std::hex << strDevice.substr(iDeviceSplit + 1, strDevice.size());
+        ssSecondStream >> memRegion.m_ucDeviceMinor;
+        ssSecondStream.clear();
+      }
+
+      ssSecondStream << std::hex << strOffset;
+      ssSecondStream >> memRegion.m_ulOffset;
+      ssSecondStream.clear();
+      ssSecondStream << strNode;
+      ssSecondStream >> memRegion.m_ulNodeFileNumber;
+
+      memRegion.m_bReadable = (strPermissions[0] == 'r');
+      memRegion.m_bWritable = (strPermissions[1] == 'w');
+      memRegion.m_bExecutable = (strPermissions[2] == 'x');
+      memRegion.m_bShared = (strPermissions[3] != '-');
+
+      if (!strPathName.empty())
+      {
+        memRegion.m_strPathName = strPathName;
+
+        std::int32_t iFileNameSplit = strPathName.find_last_of('/');
+
+        if (iFileNameSplit != -1)
+        {
+          memRegion.m_strFileName = strPathName.substr(iFileNameSplit + 1, strPathName.size());
+        }
+      }
+
+      m_prgpMemoryMap.emplace_back(memRegion);
+    }
+  }
+}
+
+//-------------------------------------------------------------------//
+const std::vector<MEMORY_MAP> &MANAGER::GetMemoryInfo()
+{
+  return m_prgpMemoryMap;
 }
 
 } // namespace FMEMORY
@@ -191,7 +289,6 @@ namespace JS
 
 // declarations
 FMEMORY::MANAGER mngrMemory;
-FMEMORY::MEMORY_MAP mapMemory;
 
 Napi::Number getProcessID(const Napi::CallbackInfo &_info)
 {
@@ -267,63 +364,63 @@ Napi::Number readMem(const Napi::CallbackInfo &_info)
   if (dataType.Utf8Value().compare("int") == 0)
   {
     int dummy = 0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("uint") == 0)
   {
     unsigned int dummy = 0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
-    else if (dataType.Utf8Value().compare("long") == 0)
+  else if (dataType.Utf8Value().compare("long") == 0)
   {
     long dummy = 0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("ulong") == 0)
   {
     unsigned long dummy = 0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("short") == 0)
   {
     short dummy = 0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("float") == 0)
   {
     float dummy = 0.0f;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("double") == 0)
   {
     double dummy = 0.0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("byte") == 0)
   {
     std::uint8_t dummy = 0;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
   else if (dataType.Utf8Value().compare("bool") == 0)
   {
     bool dummy = false;
-    mngrMemory.ReadProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.ReadProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
     return Napi::Number::New(env, dummy);
   }
 
@@ -364,55 +461,55 @@ Napi::Function writeMem(const Napi::CallbackInfo &_info)
   if (dataType.Utf8Value().compare("int") == 0)
   {
     int dummy = valueToWrite.Int32Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("uint") == 0)
   {
     unsigned int dummy = valueToWrite.Uint32Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("long") == 0)
   {
     long dummy = valueToWrite.Int64Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("ulong") == 0)
   {
     unsigned long dummy = valueToWrite.Int64Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("short") == 0)
   {
     short dummy = valueToWrite.Int32Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("float") == 0)
   {
     float dummy = valueToWrite.FloatValue();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("double") == 0)
   {
     double dummy = valueToWrite.DoubleValue();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("byte") == 0)
   {
     unsigned char dummy = valueToWrite.Uint32Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   else if (dataType.Utf8Value().compare("bool") == 0)
   {
     bool dummy = valueToWrite.Int32Value();
-    mngrMemory.WriteProcessMemory(reinterpret_cast<void*>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
+    mngrMemory.WriteProcessMemory(reinterpret_cast<void *>(baseAddress.Int64Value()), &dummy, sizeof(dummy));
   }
 
   return returnValue;
@@ -426,21 +523,50 @@ Napi::Number findSignature(const Napi::CallbackInfo &_info)
   // check if the argument is valid
   if (_info.Length() < 1 || !_info[0].IsString())
   {
+    Napi::TypeError::New(env, "Module name expected - (String)").ThrowAsJavaScriptException();
+  }
+  else if (_info.Length() < 1 || !_info[1].IsNumber())
+  {
+    Napi::TypeError::New(env, "Process id expected - (Number)").ThrowAsJavaScriptException();
+  }
+  else if (_info.Length() < 1 || !_info[2].IsString())
+  {
     Napi::TypeError::New(env, "Signature expected - (String)").ThrowAsJavaScriptException();
   }
-  else if (_info.Length() < 1 || !_info[1].IsString())
+  else if (_info.Length() < 1 || !_info[3].IsString())
   {
     Napi::TypeError::New(env, "Signature mask (pattern) expected - (String)").ThrowAsJavaScriptException();
   }
 
+  // dummy module
+  FMEMORY::MEMORY_MAP mapDummyModule;
+
+  // module name
+  Napi::String moduleName = _info[0].As<Napi::String>();
+
+  // process id
+  Napi::Number processID = _info[1].As<Napi::Number>();
+
   // signature
-  Napi::String signaturePattern = _info[0].As<Napi::String>();
+  Napi::String signaturePattern = _info[2].As<Napi::String>();
 
   // mask
-  Napi::String signatureMask = _info[1].As<Napi::String>();
+  Napi::String signatureMask = _info[3].As<Napi::String>();
+
+  // map process memory
+  mngrMemory.MapProcessMemoryRegions(processID.Int32Value());
+
+  for (const FMEMORY::MEMORY_MAP &region : mngrMemory.GetMemoryInfo())
+  {
+    if ((region.m_strFileName.compare(moduleName.Utf8Value()) == 0) && region.m_bExecutable)
+    {
+      mapDummyModule = region;
+      break;
+    }
+  }
 
   // get signature address
-  Napi::Number returnValue = Napi::Number::New(env, reinterpret_cast<std::uintptr_t>(mapMemory.FindSignature(mngrMemory, signaturePattern.Utf8Value().c_str(), signatureMask.Utf8Value().c_str())));
+  Napi::Number returnValue = Napi::Number::New(env, reinterpret_cast<std::uintptr_t>(mapDummyModule.FindSignature(mngrMemory, signaturePattern.Utf8Value().c_str(), signatureMask.Utf8Value().c_str())));
 
   return returnValue;
 }
@@ -491,7 +617,7 @@ Napi::Number getAbsoluteAddress(const Napi::CallbackInfo &_info)
   // address offset
   Napi::Number addressOffset = _info[1].As<Napi::Number>();
 
-    // address size
+  // address size
   Napi::Number addressSize = _info[2].As<Napi::Number>();
 
   // get call address
